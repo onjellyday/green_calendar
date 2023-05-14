861c51f8b504ce1ae6f8d3f28594ce266c720a9c
gyummini <133197474+gyummini@users.noreply.github.com>
2023-05-14 21:34:31 +0900

    Add files via upload

--- plant_data.py
+++ plant_data.py
@@ -2,6 +2,23 @@ import sqlite3
 import requests
 import xml.etree.cElementTree as ET
 
+# DB 생성 또는 연결
+con = sqlite3.connect('plants.db')
+cur = con.cursor()
+
+# 식물 정보 저장 테이블 생성
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS plants (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        species TEXT,
+        name TEXT,
+        temperature TEXT,
+        lowest_temp TEXT,
+        humidity TEXT,
+        light TEXT
+    )
+''')
+
 # 요청 URL 설정
 list_url = 'http://api.nongsaro.go.kr/service/garden/gardenList'
 detail_url = 'http://api.nongsaro.go.kr/service/garden/gardenDtl'
@@ -30,7 +47,29 @@ for plant in root.iter('item'):
         print('식물의 이름:', name)
         break
 else:
-    print('일치하는 식물의 이름을 찾을 수 없습니다.')
+    print('일치하는 식물의 이름을 찾을 수 없습니다. 직접 입력해 주세요')
+    species = input('식물의 종을 입력하세요: ')
+    name = input('식물의 이름을 입력하세요: ')
+    temperature = input('식물의 적정 온도를 입력하세요: ')
+    lowest_temp = input('식물의 겨울철 최소 온도를 입력하세요: ')
+    humidity = input('식물의 적정 습도를 입력하세요: ')
+    light = input('식물의 적정 조도를 입력하세요: ') #=> 웹 프로그래밍에선 선택지중 선택하도록 변경
+
+# 사용자 입력 정보 DB에 저장
+    cur.execute("INSERT INTO plants (species, name, temperature, lowest_temp, humidity, light) VALUES (?, ?, ?, ?, ?, ?)",
+            (species, name, temperature, lowest_temp, humidity, light))
+    con.commit()
+    # DB 연결 종료
+    cur.close()
+    con.close()
+    # 결과 출력
+    print(' 종:', species)
+    print(' 이름:', name)
+    print(' 온도:', temperature)
+    print(' 겨울철 최소 온도:', lowest_temp)
+    print(' 습도:', humidity)
+    print(' 조도:', light)
+
     exit()
 
 # API 호출
@@ -50,31 +89,16 @@ lowest_temp = root.find('.//winterLwetTpCodeNm').text
 humidity = root.find('.//hdCodeNm').text
 light = root.find('.//lighttdemanddoCodeNm').text
 
-# DB 생성 또는 연결
-con = sqlite3.connect('plants.db')
-cur = con.cursor()
-
-# 식물 정보 저장 테이블 생성
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS plants (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        species TEXT,
-        name TEXT,
-        temperature TEXT,
-        humidity TEXT,
-        light TEXT
-    )
-''')
-
 # 사용자 입력 정보 DB에 저장
-cur.execute("INSERT INTO plants (species, name, temperature, humidity, light) VALUES (?, ?, ?, ?, ?)",
-            (species, name, temperature, humidity, light))
+cur.execute("INSERT INTO plants (species, name, temperature, lowest_temp, humidity, light) VALUES (?, ?, ?, ?, ?, ?)",
+            (species, name, temperature, humidity, lowest_temp, light))
 con.commit()
 
 # 결과 출력
 print(' 종:', species)
 print(' 이름:', name)
 print(' 온도:', temperature)
+print(' 겨울철 최소 온도:', lowest_temp)
 print(' 습도:', humidity)
 print(' 조도:', light)
 
