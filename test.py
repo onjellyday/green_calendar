b78492804a49911999abb809ff0b303a6b37f180
gyummini <133197474+gyummini@users.noreply.github.com>
2023-06-12 12:33:01 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,39 +1,84 @@
-import requests
-import xml.etree.ElementTree as ET
-import datetime
+import sqlite3
+# sensor-2.txt 파일 경로
+file_path = 'D:/sensor-2.txt'
 
-url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
-serviceKey = 'NintYLVb0lkZfK3OI2n9UqvNbH7V5jTXlMGX5rz6UY/Z/kC4CH+j1ED2xjMiVbz8JWU7/amPPie/w/jraLWVNg=='
-current_date2 = datetime.date(2023, 6, 8)
-end_date = datetime.date.today()
-while current_date2 <= end_date:
-    A = []
-    for i in range(24):
-        A.append(f'{i:02d}00')
-    index = 0
-    count = 0
-    sum = 0
-    avg = 0
-    while index < len(A):
-        params = {
-            'serviceKey': serviceKey,
-            'pageNo': '1',
-            'numOfRows': '50',
-            'dataType': 'XML',
-            'base_date': current_date2.strftime('%Y%m%d'),
-            'base_time': A[index],
-            'nx': '61',
-            'ny': '127'
-        }
-        response = requests.get(url, params=params)
-        root = ET.fromstring(response.content)
-        sky_code = None
-        for item in root.iter("item"):
-            category = item.find("category").text
-            fcst_value = item.find("fcstValue").text
-            if category == "SKY":
-                sky_code = fcst_value
-        print("일자, 시간 : ",A[index])
-        print("하늘 상태(SKY) 값:", sky_code)
-        index += 1
-    current_date2 += datetime.timedelta(days=1)
+# 파일 열기
+with open(file_path, 'r') as file:
+    # 파일의 모든 내용 읽기
+    lines = file.readlines()
+
+# 데이터 파싱 및 날짜별 평균 계산
+data = {}
+for line in lines:
+    line = line.strip()
+    values = line.split(',')
+    date = values[0]
+    temperature = float(values[2])
+    humidity = float(values[3])
+    brightness = int(values[4])
+
+    if date not in data:
+        data[date] = {'temperature': [], 'humidity': [], 'brightness': []}
+
+    data[date]['temperature'].append(temperature)
+    data[date]['humidity'].append(humidity)
+    data[date]['brightness'].append(brightness)
+
+# 평균 계산
+averages = {}
+for date, values in data.items():
+    averages[date] = {
+        'temperature': sum(values['temperature']) / len(values['temperature']),
+        'humidity': sum(values['humidity']) / len(values['humidity']),
+        'brightness': sum(values['brightness']) / len(values['brightness'])
+    }
+
+# sensor-average.txt 파일에 저장
+output_file_path = 'D:/sensor-average.txt'
+with open(output_file_path, 'w') as output_file:
+    for date, values in averages.items():
+        output_line = f"{date},{values['temperature']:.2f},{values['humidity']:.2f},{values['brightness']:.2f}\n"
+        output_file.write(output_line)
+
+# 데이터베이스 연결
+conn = sqlite3.connect('data.db')
+cursor = conn.cursor()
+
+# 테이블 생성 쿼리
+create_table_query = '''
+CREATE TABLE IF NOT EXISTS weather (
+    datetime TEXT,
+    temperature REAL,
+    humidity REAL,
+    lux INTEGER
+);
+'''
+
+# 테이블 생성
+cursor.execute(create_table_query)
+
+# 커밋 및 연결 종료
+conn.commit()
+conn.close()
+
+# 데이터베이스 연결
+conn = sqlite3.connect('data.db')
+cursor = conn.cursor()
+
+# 텍스트 파일 읽기
+with open(output_file_path, 'r') as file:
+    lines = file.readlines()
+
+# 데이터 삽입
+for line in lines:
+    line = line.strip()  # 줄바꿈 문자 제거
+    data = line.split(',')  # 데이터 분리
+    query = '''
+    INSERT INTO weather (datetime, temperature, humidity, lux)
+    VALUES (?, ?, ?, ?);
+    '''
+    cursor.execute(query, (data[0], data[2], data[1], data[3]))
+
+# 커밋 및 연결 종료
+conn.commit()
+conn.close()
